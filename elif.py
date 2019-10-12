259356415426b7ef2e7572fd71cca47d348ee80d
siwakornnitkit <topnitkit@gmail.com>
2019-10-12 16:01:09 +0700

    Revert "Update elif.py"
    This reverts commit d6c02c9f4c5b45f22ca00942a58a904e7c7c3a6e.

--- elif.py
+++ elif.py
@@ -1,17 +1,1 @@
-sc = int(input("Your score :"))
-if sc >= 80 :
-    print("Grade A")
 elif sc >= 75 :
-    print("Grade B+")
-elif sc >= 70 :
-    print("Grade B")
-elif sc >= 65 :
-    print("Grade C+")
-elif sc >= 60 :
-    print("Grade C")
-elif sc >= 55 :
-    print("Grade D+")
-elif sc >= 50 :
-    print("Grade D")
-elif sc <  50 :
-    print("Grade F")
