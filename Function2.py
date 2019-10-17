eb364227e5e14cdae5499f161c8959eb6e47ce71
siwakornnitkit <topnitkit@gmail.com>
2019-10-17 14:28:08 +0700

    Lecture50

---
+++ Function2.py
@@ -0,0 +1,12 @@
+def plus(x,y):
+    print(x+y)
+plus(100,20)
+def minus(a,b):
+    print(a-b)
+minus(100,10)
+def multiply(o,p):
+    print(o*p)
+multiply(60,2)
+def divide(k,l):
+    print(k/l)
+divide(120,2)
